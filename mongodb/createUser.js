de3e4fccf871079cfa65f08933400415ee6ac9c5
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-07-13 09:56:05 +0530

    Add mongodb scripts

---
+++ mongodb/createUser.js
@@ -0,0 +1,16 @@
+db.createUser(
+    {
+        user: "user",
+        pwd: "user",
+        roles: [ 
+            {
+                role: "readWrite", 
+                db:"user_db"
+            },
+            {
+                role: "dbAdmin",
+                db: "user_db"
+            }
+        ]
+    }
+)
