cd4381ce770fb58d6387fb698fcb4e3ff19bae5e
Divanshu Chauhan <23524935+Divkix@users.noreply.github.com>
2022-07-27 21:09:58 +0530

    Merge Vagrantfiles repository

---
+++ vagrantfiles/PhotonOS/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Enable ssh password authentication
+echo "Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication no/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sed -i 's/.*PermitRootLogin.*/PermitRootLogin yes/' /etc/ssh/sshd_config
+systemctl reload sshd
+
+# Set Root password
+echo "Set root password"
+echo -e "admin\nadmin" | passwd root >/dev/null 2>&1
